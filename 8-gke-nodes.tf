af2fbb719a30125a18fe68f4f823bd3a417909cb
TCRD <tcrdinesh1@gmail.com>
2025-10-31 21:30:10 +0530

    machine type update

--- 8-gke-nodes.tf
+++ 8-gke-nodes.tf
@@ -19,7 +19,7 @@ resource "google_container_node_pool" "general" {
 
   node_config {
     preemptible  = false
-    machine_type = "e2-medium"
+    machine_type = "n2-standard-2"
 
     labels = {
       role = "general"
